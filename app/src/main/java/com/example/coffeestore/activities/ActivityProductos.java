574f0ffec54a0ce1d3639f2ef0e7db32dbefa438
kenethriera <kenethrr@outlook.com>
2023-06-17 16:40:20 -0500

    Se agrega funcionalidad de filtrar productos

--- app/src/main/java/com/example/coffeestore/activities/ActivityProductos.java
+++ app/src/main/java/com/example/coffeestore/activities/ActivityProductos.java
@@ -7,10 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.example.coffeestore.R;
 import com.example.coffeestore.adapters.ProductAdapter;
 import com.example.coffeestore.database.ProductDbHelper;
+import com.example.coffeestore.dto.CategoriaProducto;
 import com.example.coffeestore.dto.Producto;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -53,7 +58,7 @@ public class ActivityProductos extends AppCompatActivity {
 
                 List<Producto> productosFiltrados = new ArrayList<>();
                 for (Producto producto : productos) {
-                    if (producto.getTitulo().contains(textoIngresado)) {
+                    if (producto.getTitulo().toUpperCase().contains(textoIngresado.toUpperCase())) {
                         productosFiltrados.add(producto);
                     }
                 }
@@ -66,5 +71,57 @@ public class ActivityProductos extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
+
+        ImageButton buttonFilterProducts = findViewById(R.id.button_filterproducts);
+        buttonFilterProducts.setOnClickListener(v -> {
+            PopupMenu popupMenu = new PopupMenu(ActivityProductos.this, v);
+            popupMenu.inflate(R.menu.menu_filter);
+
+            // Establecer listener para manejar las selecciones del menú emergente
+            popupMenu.setOnMenuItemClickListener(item -> {
+                // Aquí puedes obtener el ID del elemento seleccionado y realizar las acciones necesarias
+                int itemId = item.getItemId();
+                if (itemId == R.id.filter_bebida_caliente) {
+                    ProductAdapter productAdapter1 = new ProductAdapter(
+                            getProductosFiltradosPorCategoria(CategoriaProducto.BEBIDA_CALIENTE),
+                            ActivityProductos.this);
+                    recyclerView.setAdapter(productAdapter1);
+                } else if (itemId == R.id.filter_bebida_fria) {
+                    ProductAdapter productAdapter1 = new ProductAdapter(
+                            getProductosFiltradosPorCategoria(CategoriaProducto.BEBIDA_FRIA),
+                            ActivityProductos.this);
+                    recyclerView.setAdapter(productAdapter1);
+                } else if (itemId == R.id.filter_panes) {
+                    ProductAdapter productAdapter1 = new ProductAdapter(
+                            getProductosFiltradosPorCategoria(CategoriaProducto.PANES),
+                            ActivityProductos.this);
+                    recyclerView.setAdapter(productAdapter1);
+                } else if (itemId == R.id.filter_pasteles) {
+                    ProductAdapter productAdapter1 = new ProductAdapter(
+                            getProductosFiltradosPorCategoria(CategoriaProducto.PASTELES),
+                            ActivityProductos.this);
+                    recyclerView.setAdapter(productAdapter1);
+                } else {
+                    ProductAdapter productAdapter1 = new ProductAdapter(
+                            productos,
+                            ActivityProductos.this);
+                    recyclerView.setAdapter(productAdapter1);
+                }
+                return true;
+            });
+
+            // Mostrar el menú emergente
+            popupMenu.show();
+        });
+    }
+
+    private List<Producto> getProductosFiltradosPorCategoria(CategoriaProducto categoria) {
+        List<Producto> productosFiltrados = new ArrayList<>();
+        for (Producto producto : productos) {
+            if (producto.getCategoria().equals(categoria)) {
+                productosFiltrados.add(producto);
+            }
+        }
+        return productosFiltrados;
     }
 }
